d974c99d62dba73d2cd79efe121ffb6595107f09
hustccc <1276675421@qq.com>
2021-02-08 06:55:22 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+//! A crate for generating Latex Resume documents programatically.
+//!
+//! This library tries to create simple and useful interface to 
+//! help you writting your resume with `rustlang`.  
+//! 
+//! Imagine that it' s such a cool thing that writting your resume by programming!  
+//! 
