3e5975127ab9b52a7894ec80df63c9ef4f7929bf
hustccc <1276675421@qq.com>
2021-02-08 11:10:48 +0900

    add some traits

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,41 @@
 //! 
 //! Imagine that it' s such a cool thing that writting your resume by programming!  
 //! 
+
+/// The resume, included `ResumeClass` 
+/// and some object implemented `ResumeElement` trait
+pub struct Resume {
+    class: ResumeClass,
+    elements: Vec<Box<dyn ResumeElement>>
+}
+
+/// Classification of Resume
+pub enum ResumeClass {
+    Programmer,
+    Other,
+}
+
+pub trait ResumeElement {
+    fn title(&self) -> String;
+    fn inner(&self) -> Box<dyn Inner>;
+}
+
+
+pub trait Inner {
+    fn time(&self) -> Box<dyn Time>;
+    fn situation(&self) -> Box<dyn Situation>;
+}
+
+pub trait Time {
+    fn year(&self) -> Option<u32>;
+    fn month(&self) -> Option<String>;
+    fn day(&self) -> Option<u32>;
+}
+
+pub trait Situation {
+    fn galaxy(&self) -> Option<String>;
+    fn planet(&self) -> Option<String>;
+    fn country(&self) -> Option<String>;
+    fn province(&self) -> Option<String>;
+    fn city(&self) -> Option<String>;
+}
